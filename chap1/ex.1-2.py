8d553f9eafd008b05fad9ce1f85eeabb922730a6
Taka <103984069+Taka0007@users.noreply.github.com>
2023-05-16 15:23:32 +0900

    fin

--- chap1/ex.1-2.py
+++ chap1/ex.1-2.py
@@ -1,1 +1,22 @@
-temp
+# 3x^2-2x-4の解を2^(-k)の精度で求める
+
+def f(x):
+    return 3*(x**2)-2*x-4
+
+k = int(input())
+error = 2**(-k)
+
+left  = -(10**3)
+right = (10**3)
+mid   = (left+right)/2
+
+while abs(f(mid))>error:
+    if f(mid)>0:
+        right = mid
+        mid   = (left+right)/2
+        
+    else:
+        left = mid
+        mid  = (left+right)/2
+        
+print(mid)
